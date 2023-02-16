dfe09045943ccf3dff8621a1d507997a9669285e
msimon987 <mario.shimon87@gmail.com>
2023-02-16 19:00:01 +0100

    gsdfgs

---
+++ radSTextom.js
@@ -0,0 +1,14 @@
+var text = ' Lorem ipusm dolor sit amet.';
+
+var lenght = text.length;
+//console.log("duljina teksta = " + lenght);
+
+var wordSit = text.substring(19,22);
+//console.log(wordSit)
+
+var text = text.replace('amet', 'elit')
+//console.log(text)
+
+var newText = text + ', consecectetur adispicitin elit'
+console.log(text)
+return;
